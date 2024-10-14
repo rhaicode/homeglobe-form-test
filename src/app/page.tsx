2863f54c23dad87f3fa5fb0604d8f07dd88c639d
rhaicode <rhaidzsalali@gmail.com>
2024-10-14 11:48:47 +0800

    Additional style fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,14 +12,14 @@ export default function Home() {
     <main className='bg-lavender relative h-screen'>
       <div className='h-full sm:flex sm:items-center sm:justify-center sm:overflow-hidden'>
         <div className='pt-12 sm:pt-0'>
-          <Card className='relative z-20 mx-auto px-4 py-6 sm:mx-0 sm:w-[438px] sm:h-[562px]'>
+          <Card className='relative z-20 mx-auto px-4 py-6 sm:mx-0 sm:w-[438px] sm:h-[562px] sm:p-8'>
             <Image src={iconHomeGlobe}
               width={177.44}
               height={28}
               alt="Homeglobe" />
-            <span className='text-rich-black text-xl font-bold block my-4'>Find places to stay anywhere</span>
+            <span className='text-rich-black text-xl font-bold block my-4 sm:text-2xl'>Find places to stay anywhere</span>
             <p className='text-slate-gray'>Discover entire homes and rooms perfect for any trip or special occasion.</p>
-            <form className='mt-6 flex flex-col gap-6'>
+            <form className='mt-6 sm:mt-4 flex flex-col gap-6'>
               <Input type="text" label="Location" placeholder="Anywhere" />
               <div className='flex gap-4'>
                 <Input type="text" label="From" placeholder="Add date" />
--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -7,8 +7,8 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
 const Input: React.FC<InputProps> = ({ label, className, ...props }) => {
     return (
         <div>
-            {label && <label>{label}</label>}
-            <input className={`block mt-2 h-[45px] border-[3px] border-transparent bg-cloud-mist hover:bg-pale-fog rounded-lg p-2 w-full text-midnight-slate focus:outline-none focus:border-light-purple ${className}`} {...props} />
+            {label && <label className='text-rich-black text-sm'>{label}</label>}
+            <input className={`block mt-1 h-[45px] border-[3px] border-transparent bg-cloud-mist hover:bg-pale-fog rounded-lg p-2 w-full text-midnight-slate focus:outline-none focus:border-light-purple ${className}`} {...props} />
         </div>
     );
 };
