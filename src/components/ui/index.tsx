ce874b31f7017f4650d9ee492255cf3c38f211fc
rhaicode <rhaidzsalali@gmail.com>
2024-10-14 11:20:56 +0800

    UI responsiveness

---
+++ src/components/ui/index.tsx
@@ -0,0 +1,7 @@
+import Autocomplete from "./Autocomplete"
+import Button from "./Button"
+import Card from "./Card"
+import Input from "./Input"
+
+
+export { Autocomplete, Button, Card, Input }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-        "vivid-violet": "#7749EF",
-        "electric-violet": "#5C27E5",
-        "light-purple": "#AF72FF",
-        "midnight-slate": "#1F2A37",
-        "cloud-mist": "#F3F4F6",
-        "pale-fog": "#E5E7EB",
+        'vivid-violet': '#7749EF',
+        'electric-violet': '#5C27E5',
+        'light-purple': '#AF72FF',
+        'midnight-slate': '#1F2A37',
+        'cloud-mist': '#F3F4F6',
+        'pale-fog': '#E5E7EB',
+        lavender: '#F7F4FF',
+        'rich-black': '#111928',
+        'slate-gray': '#4B5563',
+      },
+      screens: {
+        xl2: '1440px',
       },
     },
   },
